15d4bf5071eca870d08c6105b5c9b246625600a6
Juzekkk <juzekgaming@gmail.com>
2023-04-06 13:49:54 +0200

    Deleted unnecessary stuff from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -16,15 +16,6 @@
 #include <cmath>
 #include <cstdlib>
 
-void clearConsole()
-{
-#ifdef _WIN32
-    std::system("CLS");
-#else
-    std::system("clear");
-#endif
-}
-
 void modifyLogAlternation(std::vector<float> &vec)
 {
     for (int i = 0; i < vec.size(); i++)
@@ -97,90 +88,3 @@ int main()
 
     return 0;
 }
-
-// #include <iostream>
-// #include <GLFW/glfw3.h>
-// #include "TransparentWindow.h"
-
-// int main()
-// {
-//     TransparentWindow transparentWindow;
-//     transparentWindow.createWindow();
-//     GLFWwindow *window = transparentWindow.getWindow();
-
-//     // Set the initial bar heights
-//     std::vector<float> barHeights = {0.2f, 0.5f, 0.3f, 0.7f, 0.4f, 0.6f, 0.1f};
-//     transparentWindow.setBarHeights(barHeights);
-
-//     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-//     {
-//         std::cerr << "Failed to initialize GLAD" << std::endl;
-//         return -1;
-//     }
-
-//     while (!glfwWindowShouldClose(window))
-//     {
-//         glfwPollEvents();
-
-//         int display_w, display_h;
-//         glfwGetFramebufferSize(window, &display_w, &display_h);
-//         glViewport(0, 0, display_w, display_h);
-//         glClearColor(0.45f, 0.55f, 0.60f, 0.7f); // Set background color with transparency
-//         glClear(GL_COLOR_BUFFER_BIT);
-
-//         transparentWindow.draw();
-
-//         glfwSwapBuffers(window);
-//     }
-
-//     glfwDestroyWindow(window);
-//     glfwTerminate();
-
-//     return 0;
-// }
-
-// int main()
-// {
-//     AudioCapture audioCapture;
-
-//     if (!audioCapture.initialize())
-//     {
-//         std::cerr << "Failed to initialize AudioCapture" << std::endl;
-//         return 1;
-//     }
-
-//     audioCapture.startCapture();
-
-//     unsigned int numberOfWindows = 12; // Change this to the desired number of frequency windows
-//     AudioProcessor audioProcessor(numberOfWindows, audioCapture);
-//     audioProcessor.startProcessing();
-
-//     const unsigned int displayIntervalMs = 10;
-//     const int maxBarLength = 40;
-
-//     while (true)
-//     {
-//         std::vector<float> frequencyWindowMagnitudes = audioProcessor.getFrequencyWindowMagnitudes();
-//         modifyLogAlternation(frequencyWindowMagnitudes);
-//         if (!frequencyWindowMagnitudes.empty())
-//         {
-//             clearConsole();
-//             for (float magnitude : frequencyWindowMagnitudes)
-//             {
-//                 int barLength = static_cast<int>(std::round(magnitude * maxBarLength));
-//                 for (int i = 0; i < barLength; ++i)
-//                 {
-//                     std::cout << '#';
-//                 }
-//                 std::cout << std::endl;
-//             }
-//         }
-
-//         std::this_thread::sleep_for(std::chrono::milliseconds(displayIntervalMs));
-//     }
-
-//     audioProcessor.stopProcessing();
-//     audioCapture.stopCapture();
-
-//     return 0;
-// }
